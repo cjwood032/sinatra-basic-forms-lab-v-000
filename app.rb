7317e90c0c4d148419167e98ec67f2a640326fba
Christopher Wood <cjwood032@gmail.com>
2018-07-23 14:35:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,10 @@ class App < Sinatra::Base
   get '/new' do
     erb :create_puppy
   end
-  post '/' do
+  post '/puppy' do
+    @name = params['name']
+    @breed = params['breed']
+    @age = params['age']
     erb :display_puppy
   end
 end
